9c9e372a1bf4c1fe0149b9cf0ec231b54af17e5a
Katarina Shaw <KatarinaShaw@Katarina-Shaws-MacBook.local>
2014-06-08 17:08:10 -0400

    created new SDFB rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sdfb::Application.config.secret_token = 'e55c577db5b740530f192e14698a61ce49b61ee9b325cf1ccb1dc21b55a5b59989d09351b383a7f17c34f3a377d0a1663fd19e4cae9156d6037b45dc4fa9e69c'
