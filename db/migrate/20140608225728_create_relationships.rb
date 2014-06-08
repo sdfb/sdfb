ee76cf303386f6928695fc4d891f6cbcf4b9143f
Katarina Shaw <KatarinaShaw@Katarina-Shaws-MacBook.local>
2014-06-08 19:06:00 -0400

    scaffolded User, User_rel_contrib, Relationship, Person, Group

---
+++ db/migrate/20140608225728_create_relationships.rb
@@ -0,0 +1,11 @@
+class CreateRelationships < ActiveRecord::Migration
+  def change
+    create_table :relationships do |t|
+      t.integer, :person1_index
+      t.integer, :person2_index
+      t.decimal :original_certainty
+
+      t.timestamps
+    end
+  end
+end
