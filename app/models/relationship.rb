ec6d757c9d677932f4a32e20dbfa473327280203
Katarina Shaw <KatarinaShaw@Katarina-Shaws-MacBook.local>
2014-06-08 20:45:13 -0400

    deleting

--- app/models/relationship.rb
+++
@@ -1,3 +0,0 @@
-class Relationship < ActiveRecord::Base
-  attr_accessible :original_certainty, :person1_index, :person2_index
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Sdfb::Application.config.secret_token = 'de0f2081b723407cf2c56604d19958df547e20dc72fe1ec9fda27f1114ab60a7ce6effe5ca2821d4f8700829e1e479cc19fe26ed4c60cd0e10be860a8feff510'
