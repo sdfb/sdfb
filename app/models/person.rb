2023e7bd9e7c6ff6273447884926dc9f11633de0
Katarina Shaw <KatarinaShaw@Katarina-Shaws-MacBook.local>
2014-06-09 19:43:33 -0400

    added has_many :relationship to Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ActiveRecord::Base
   attr_accessible :birth_year, :created_by, :death_year, :first_name, :historical_significance, :last_name, :original_id
+  has_many :relationships
 end
