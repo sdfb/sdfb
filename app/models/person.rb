f06b6ce344fc335e1ceee8197ed2f1e71588649a
Katarina Shaw <KatarinaShaw@Katarina-Shaws-MacBook.local>
2014-06-08 20:45:13 -0400

    deleting

--- app/models/person.rb
+++
@@ -1,3 +0,0 @@
-class Person < ActiveRecord::Base
-  attr_accessible :birth_year, :created_by, :death_year, :historical_significance, :name
-end
--- app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :about_description, :affiliation, :email, :first_name, :is_active, :is_admin, :last_name, :password_hash, :password_salt, :user_type
-end
