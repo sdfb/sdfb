4215793666f25d464da5e19106115393904c298d
Katarina Shaw <KatarinaShaw@Katarina-Shaws-MacBook.local>
2014-06-15 16:07:52 -0400

    added log_out, added sign up and log in links, refused access to password_hash and password_salt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,4 +12,9 @@ class SessionsController < ApplicationController
       render "new"
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Logged out!"
+  end
 end
