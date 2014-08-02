4f2659763f511b61b4b5f33424bed4851d6ed19c
Katarina Shaw <KatarinaShaw@Katarina-Shaws-MacBook.local>
2014-08-02 16:52:57 -0400

    added check to see if there is a user signed in to avoid errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+  	# gon.people = Person.all_approved
+  end
+  def test
+  	gon.people = Person.all_approved
+  	gon.test = view_context.asset_path 'data.json'
   end
 end
