6fabbd767347d695b8bfd7bf8a3f5bf60cd18642
Katarina <katarinashaw@gmail.com>
2015-01-01 13:36:55 -0500

    added prefix, suffix, and title fields to people

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	#gon.people = Person.all_approved
   	gon.people = Person.find_2_degrees_for_person(params[:person_id])
-  	gon.people_list = Person.select("id, first_name, last_name, ext_birth_year")
+  	gon.people_list = Person.select("id, first_name, last_name, ext_birth_year, prefix, suffix, title")
   end
   def test
   	#gon.people = Person.find_2_degrees_for_person(params[:person_id])
