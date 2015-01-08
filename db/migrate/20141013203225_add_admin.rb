0da7dd3c1c094173dc9d78cccafb7c1d4ec76ecf
Katarina <katarinashaw@gmail.com>
2015-01-08 01:47:24 -0500

    updated admin passwords

--- db/migrate/20141013203225_add_admin.rb
+++ db/migrate/20141013203225_add_admin.rb
@@ -4,8 +4,8 @@ class AddAdmin < ActiveRecord::Migration
     admin.first_name = "Katarina"
     admin.last_name = "Shaw"
     admin.email = "admin@example.com"
-    admin.password = "admin"
-    admin.password_confirmation = "admin"
+    admin.password = "admin@FrancisBacon1"
+    admin.password_confirmation = "admin@FrancisBacon1"
     admin.user_type = "Admin"
     admin.is_active = true
     admin.username = "KatAdmin"
@@ -16,8 +16,8 @@ class AddAdmin < ActiveRecord::Migration
     odnb_admin.first_name = "ODNB"
     odnb_admin.last_name = "Admin"
     odnb_admin.email = "odnb_admin@example.com"
-    odnb_admin.password = "admin"
-    odnb_admin.password_confirmation = "admin"
+    odnb_admin.password = "admin@FrancisBacon1"
+    odnb_admin.password_confirmation = "admin@FrancisBacon1"
     odnb_admin.user_type = "Admin"
     odnb_admin.is_active = true
     odnb_admin.username = "ODNB_Admin"
@@ -28,8 +28,8 @@ class AddAdmin < ActiveRecord::Migration
     sdfb_admin.first_name = "SDFB"
     sdfb_admin.last_name = "Admin"
     sdfb_admin.email = "sdfb_admin@example.com"
-    sdfb_admin.password = "admin"
-    sdfb_admin.password_confirmation = "admin"
+    sdfb_admin.password = "admin@FrancisBacon1"
+    sdfb_admin.password_confirmation = "admin@FrancisBacon1"
     sdfb_admin.user_type = "Admin"
     sdfb_admin.is_active = true
     sdfb_admin.username = "SDFB_Admin"
