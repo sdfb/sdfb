2cb15adbcd4ed7f1c96517e590a76bba23af65bf
Ally Sorge <allysorge@Allys-MBP.wv.cc.cmu.edu>
2015-01-14 12:37:13 -0500

    Changing variables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
-    gon.people = [[1], [2]]
-    gon.people_list = [[1], [2]]
+   # gon.people = [[1], [2]]
+   # gon.people_list = [[1], [2]]
+   @people = Person.find_first_degree_for(params[:id])
+   gon.people = @people 
   	#gon.people = Person.find_first_degree_for(params[:id])
   	#gon.people_list = Person.select("id, first_name, last_name, ext_birth_year, prefix, suffix, title")
   end
