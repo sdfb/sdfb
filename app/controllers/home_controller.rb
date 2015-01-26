9603b677eeffb62fe055a3cb35662c3d3a25e7cf
Katarina <katarinashaw@gmail.com>
2015-01-25 22:00:56 -0500

    passing through gon.group_data for the visualization group search panel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     @people = Person.find_first_degree_for(params[:id])
     gon.people = @people 
   	gon.people_list = Person.select("id, first_name, last_name, ext_birth_year, prefix, suffix, title")
+  	gon.group_data = Group.all_approved.select("id, name, description, person_list")
   end
   def test
   	#gon.people = Person.find_2_degrees_for_person(params[:person_id])
