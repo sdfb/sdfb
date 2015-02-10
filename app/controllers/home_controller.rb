4e6b1efb37b4b1c255f4b7c888ef6429e03737a4
Katarina <katarinashaw@gmail.com>
2015-02-09 22:14:03 -0500

    added rails autocomplete to homepage and took password off of development

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  autocomplete :person, :search_names_all, full: true, :extra_data => [:first_name, :last_name, :ext_birth_year], :display_value => :autocomplete_name
   def index
     #If there are no relationships, only return the person node
     # @people = Person.find_first_degree_for(params[:id])
@@ -12,12 +13,15 @@ class HomeController < ApplicationController
         @data['people'] = Person.find(params[:id])
       end
     end
-    # @data['people'] = Person.find(10000473)
-    @data['all_people'] = Person.all_approved.select("id, first_name, last_name, ext_birth_year, prefix, suffix, title")
+    # @data['all_people'] = Person.all_approved.select("id, first_name, last_name, ext_birth_year, prefix, suffix, title")
     @data['group_data'] = Group.all_approved.select("id, name, description, person_list")
   end
   def list
 
+  end
+
+  def get_autocomplete_items(parameters)
+    active_record_get_autocomplete_items(parameters).where("approved_by is not null and is_active is true and is_rejected is false")
   end
 
   def test
