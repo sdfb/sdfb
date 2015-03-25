6fb5f3f7b7633b1c777696b4eed8a6e6c0be5163
Katarina <katarinashaw@gmail.com>
2015-03-25 14:54:44 -0400

    testing which callback causes bug in adding user_rel_contrib

--- app/models/user_rel_contrib.rb
+++ app/models/user_rel_contrib.rb
@@ -58,7 +58,7 @@ class UserRelContrib < ActiveRecord::Base
   before_create :autocomplete_to_rel
   before_update :add_editor_update_max_cert_check_approved
   # before_create :update_max_certainty
-  #after_create :check_if_approved
+  after_create :check_if_approved
   #after_create :create_relationship_types_list
   after_update :create_relationship_types_list
 
