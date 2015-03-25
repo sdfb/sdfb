526b27236d7fe818cc1ac95646905f4242472add
Jeremy Lee <germyjermy@yahoo.com>
2015-03-24 20:00:37 -0400

    fixed accordion

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -6,24 +6,44 @@ var default_edate = 1800;
   
 /*
 Num_rels Cluster    Color
- 0-1		0        orange
- 2-3		1        yellow
- 4-5		2        aqua blue
- 6-7		3        marine blue
- 8-9		4        light blue
- 10-11	5        hot pink
- 12+		6        purple
-
+ 0-1    0        orange
+ 2-3    1        yellow
+ 4-5    2        aqua blue
+ 6-7    3        marine blue
+ 8-9    4        light blue
+ 10-11  5        hot pink
+ 12+    6        purple
 */
 
 // //returns cluster number based on number of relationships the cluster has
+/*
 function getClusterRels(node){
-	try{
-		var size = Object.keys(node).length;
-	}
-	catch(err) {
-		var size = 0;
-	}
+  try{
+    var size = Object.keys(node).length;
+  }
+  catch(err) {
+    var size = 0;
+  }
+
+  if (size > 100){
+    return 10;
+  }else{
+    return (Math.floor(size / 2)) / 10;
+  }
+}
+//returns colors based on cluster group number
+function getColorsRels(){
+   return { 0: "#bf5b17", 1: "#d73027", 2: "#fc8d59", 3: "#fee090", 4: "#e0f3f8", 5: "#91bfdb", 6: "#4575b4"}
+}
+*/
+//returns cluster number based on number of relationships the cluster has
+ function getClusterRels(node){
+   try{
+     var size = Object.keys(node).length;
+   }
+   catch(err) {
+     var size = 0;
+   }
 
    if (size >= 50){
      return 6;
@@ -35,83 +55,17 @@ function getClusterRels(node){
      return 3;
    } else if (size >=5) {
      return 2;
-   } else if (size >=1) {
+   } else if (size >1) {
      return 1;
    } else {
      return 0;
    }
-}
-//returns colors based on cluster group number
-function getColorsRels(){
-   return { 0: "#bdbdbd", 1: "#d73027", 2: "#fc8d59", 3: "#fee090", 4: "#abd9e9", 5: "#74add1", 6: "#4575b4"}
-}
+ }
 
-//returns cluster number based on number of relationships the cluster has
-
-// function getClusterRels(node){
-// 	try{
-// 		var size = Object.keys(node).length;
-// 	}
-// 	catch(err) {
-// 		var size = 0;
-// 	}
-
-//   if (size >= 50){
-//     return 6;
-//   } else if (size >= 30){
-//     return 5;
-//   } else if (size >= 20) {
-//     return 4;
-//   } else if (size >=10) {
-//     return 3;
-//   } else if (size >=5) {
-//     return 2;
-//   } else if (size >=1) {
-//     return 1;
-//   } else {
-//     return 0;
-//   }
-// 	if (size > 100){
-// 		return 10;
-// 	}else{
-// 		return (Math.floor(size / 2)) / 10;
-// 	}
-// }
 // //returns colors based on cluster group number
-// function getColorsRels(){
-//    return { 0: "#bf5b17", 1: "#d73027", 2: "#fc8d59", 3: "#fee090", 4: "#e0f3f8", 5: "#91bfdb", 6: "#4575b4"}
-// }
-
-//returns cluster number based on number of relationships the cluster has
-function getClusterRels(node){
-  try{
-    var size = Object.keys(node).length;
-  }
-  catch(err) {
-    var size = 0;
-  }
-
-  if (size >= 50){
-    return 6;
-  } else if (size >= 30){
-    return 5;
-  } else if (size >= 20) {
-    return 4;
-  } else if (size >=10) {
-    return 3;
-  } else if (size >=5) {
-    return 2;
-  } else if (size >1) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-//returns colors based on cluster group number
-function getColorsRels(){
-   return { 0: "#bdbdbd", 1: "#d73027", 2: "#fc8d59", 3: "#fee090", 4: "#abd9e9", 5: "#74add1", 6: "#4575b4"}
-}
+ function getColorsRels(){
+    return { 0: "#bdbdbd", 1: "#d73027", 2: "#fc8d59", 3: "#fee090", 4: "#abd9e9", 5: "#74add1", 6: "#4575b4"}
+ }
 
 // Checks if a value is in an array
 function notInArray(arr, val) {
@@ -129,22 +83,20 @@ function notInArray(arr, val) {
 
 //creates a nodekey associative array with node info
 function createNodeKey(node, id) {
-  return {"text": node["display_name"].replace(" ", "_"), "size": 10, "id": id,  "cluster": getClusterRels(node["rel_sum"])};
-
+  return {"text": node["display_name"], "size": 10, "id": id,  "cluster": getClusterRels(node["rel_sum"])};
 }
 
 function twoDegs(id, id2, people) {
-	var keys = {};
-	var edges = [];
+  var keys = {};
+  var edges = [];
   var nodes = [];
-	function createGraph(id, people) {
-  	var p = people[id];
-  	$.each(p.rel_sum, function(index, value) { 
-  	  var q = value[0];
-  		keys[q] = createNodeKey(people[q],q);
-      console.log(keys[q]);
-  		if (notInArray(edges, [id, value[0]])) {
-  		 edges.push([id, value[0]]);
+  function createGraph(id, people) {
+    var p = people[id];
+    $.each(p.rel_sum, function(index, value) { 
+      var q = value[0];
+      keys[q] = createNodeKey(people[q],q);
+      if (notInArray(edges, [id, value[0]])) {
+       edges.push([id, value[0]]);
       }
       $.each(people[q].rel_sum, function(index, value1) { 
         var r = value1[0];
@@ -157,7 +109,7 @@ function twoDegs(id, id2, people) {
       });
     });
     //adds main person's id referenced to keys associative array. Keys represent all data in graph
-    keys[id] = {"text": p["display_name"].replace(" ", "_"), "size": 30, "id": id,  "cluster": getClusterRels(p["rel_sum"])}; 
+    keys[id] = {"text": p["display_name"], "size": 30, "id": id,  "cluster": getClusterRels(p["rel_sum"])}; 
   }
   createGraph(id, people);
   if (id2 != 0 && id2 != ""){
@@ -189,7 +141,7 @@ function twoDegs(id, id2, people) {
           });  
       accordion("node");
     });
-	
+  
     graph.on("edge:click", function(d) {
         var id1 = parseInt(d.source.id);
         var id2 = parseInt(d.target.id);
@@ -205,7 +157,7 @@ function twoDegs(id, id2, people) {
           });  
         accordion("edge");
     });
-    graph.tooltip("<div class='btn' >"+"{{text}}".replace("_", " ") + "</div>");
+    graph.tooltip("<div class='btn' >"+"{{text}}".replace(/\%20/," ") + "</div>");
     $('#zoom button.icon').click(function(e){
         if (this.name == 'in') {
             graph.zoomIn();
@@ -288,20 +240,25 @@ function initGraph(people){
   $("#results").html("Two degrees of " + name + name2 + " at " + confidence + "% from " + date);
   //click methods for all the 'find' buttons in the search bar
   //this should not use the entire peopletoarray instead it should use whatever value is passed through by the #one
+}
+
+function sidebarSearch(people){
   $("#search-network-submit").click(function () {
-  	var table = 'no';
+    var table = 'no';
     Pace.restart();
     // make the index equal autocomplete
     var id = $("#search-network-name-id").val();
     if ($("#show-table").val() == 1) {
-    	table = 'yes'
+      table = 'yes'
     }
     var confidence = $("#search-network-slider-confidence-result-hidden").val().split(" - ");
     var date = $("#search-network-slider-date-result-hidden").val().split(" - ");
-    window.location.href = '/?id=' + id + '&confidence=' + confidence + '&date=' + date + '&table=' + table;
+    if (id  && confidence && date ){
+      window.location.href = '/?id=' + id + '&confidence=' + confidence + '&date=' + date;
+    }
   });
 
-   $("#search-shared-network-submit").click(function () {
+   $("#search-shared-network-submit").click(function () {
       var table = 'no';
       Pace.restart();
       if ($("#search-shared-network-name1-id").val()) {
@@ -315,14 +272,18 @@ function initGraph(people){
       }
       var confidence = $("#search-shared-network-slider-confidence-result-hidden").val().split(" - ");
       var date = $("#search-shared-network-slider-date-result-hidden").val().split(" - ");
-      window.location.href = '/?id=' + id1 + '&id2=' + id2 + '&confidence=' + confidence + '&date=' + date + '&table=' + table;
+      if (id1 && id2 && date && confidence){
+        window.location.href = '/?id=' + id1 + '&id2=' + id2 + '&confidence=' + confidence + '&date=' + date + '&table=' + table;
+      }    
     });
 
   $("#search-group-submit").click(function () {
     Pace.restart();
     // make the index equal autocomplete
     var id = $("#search-group-name-id").val();
-    window.location.href = '/?group=' + id;
+    if (id ){
+      window.location.href = '/?group=' + id;
+    }
   });
 
   $("#search-shared-group-submit").click(function () {
@@ -330,7 +291,9 @@ function initGraph(people){
     // make the index equal autocomplete
     var id1 = $("#search-shared-group-name1-id").val();
     var id2 = $("#search-shared-group-name2-id").val();
-    window.location.href = '/?group=' + id1 + '&group2=' + id2;
+    if (id1 && id2){
+      window.location.href = '/?group=' + id1 + '&group2=' + id2;
+    }  
   });
 
   $("#nav-filter-submit").click(function (){
@@ -366,25 +329,26 @@ function initGraph(people){
 }
 
 function init() {
-	var people = window.gon.people;
-	var group_data = window.gon.group_data;
+  var people = window.gon.people;
+  var group_data = window.gon.group_data;
   var group = window.gon.group;
   var group2 = window.gon.group2;
   var group_members = window.gon.group_members;
+  sidebarSearch(people);
   if (getParam("group").length == 0){
-  	filterGraph(people);
-  	initGraph(people);
-  	$('#group-table').hide();
+    filterGraph(people);
+    initGraph(people);
+    $('#group-table').hide();
   }else{
-     accordion("group"); 	
-  	$("#filterBar").hide();
-  	$("#group-name").text(group["name"]);
-  	$("#group-description").text(group["description"]);
-  	if (typeof group2 != 'undefined'){
-	  	$("#group-name2").text(group2["name"]);
-	  	$("#group-description2").text(group2["description"]);
-	  	$(".group2").show();
-	  }
+     accordion("group");  
+    $("#filterBar").hide();
+    $("#group-name").text(group["name"]);
+    $("#group-description").text(group["description"]);
+    if (typeof group2 != 'undefined'){
+      $("#group-name2").text(group2["name"]);
+      $("#group-description2").text(group2["description"]);
+      $(".group2").show();
+    }
     $.each(group_members, function(index, value) { 
       $( "#group-table" ).append( "<div class='group-row'><div class='col-md'>" + group_members[index]["display_name"] + "</div><div class='col-md'>" + group_members[index]["ext_birth_year"] + "</div><div class='col-md'>" + group_members[index]["ext_death_year"] + "</div><div class='col-md'><a href='/people/" + group_members[index]["id"] + "'>" + + group_members[index]["id"] + "</a></div></div>");
     });
