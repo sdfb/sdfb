7ab858c94b150d044e02ecc632acc584fe3106e8
Ally Sorge <asorge@andrew.cmu.edu>
2015-05-28 04:31:06 -0500

    changing herokupop script for sandbox app

--- herokupop.sh
+++ herokupop.sh
@@ -9,11 +9,11 @@ heroku run rake db:populate_group_cat_assigns --app sdfb2
 heroku run rake db:populate_rels_2_20000 --app sdfb2
 heroku run rake db:populate_rels_20001_40000 --app sdfb2
 heroku run rake db:populate_rels_40001_60000 --app sdfb2
-heroku run rake db:populate_rels_60001_80000 --app sdfb2
-heroku run rake db:populate_rels_80001_100000 --app sdfb2
-heroku run rake db:populate_rels_100001_120000 --app sdfb2
-heroku run rake db:populate_rels_120001_140000 --app sdfb2
-heroku run rake db:populate_rels_140001_160000 --app sdfb2
-heroku run rake db:populate_rels_160001_170542 --app sdfb2
+# heroku run rake db:populate_rels_60001_80000 --app sdfb2
+# heroku run rake db:populate_rels_80001_100000 --app sdfb2
+# heroku run rake db:populate_rels_100001_120000 --app sdfb2
+# heroku run rake db:populate_rels_120001_140000 --app sdfb2
+# heroku run rake db:populate_rels_140001_160000 --app sdfb2
+# heroku run rake db:populate_rels_160001_170542 --app sdfb2
 heroku run rake db:populate_user_rel_contribs_samples --app sdfb2
 heroku run rake db:populate_people_search_names_all --app sdfb2
