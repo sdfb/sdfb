3b14553ec9e636c861cb20e8896c9c9fabfcb132
Ally Sorge <asorge@andrew.cmu.edu>
2015-05-28 04:38:46 -0500

    Correction with populating script

--- herokupop.sh
+++ herokupop.sh
@@ -6,6 +6,8 @@ heroku run rake db:populate_rel_cats --app sdfb2
 heroku run rake db:populate_rel_types --app sdfb2
 heroku run rake db:populate_rel_cat_assigns --app sdfb2
 heroku run rake db:populate_group_cat_assigns --app sdfb2
+heroku run rake db:populate_people_display_names --app sdfb2
+heroku run rake db:populate_people_search_names_all --app sdfb2
 heroku run rake db:populate_rels_2_20000 --app sdfb2
 heroku run rake db:populate_rels_20001_40000 --app sdfb2
 heroku run rake db:populate_rels_40001_60000 --app sdfb2
@@ -16,4 +18,7 @@ heroku run rake db:populate_rels_40001_60000 --app sdfb2
 # heroku run rake db:populate_rels_140001_160000 --app sdfb2
 # heroku run rake db:populate_rels_160001_170542 --app sdfb2
 heroku run rake db:populate_user_rel_contribs_samples --app sdfb2
-heroku run rake db:populate_people_search_names_all --app sdfb2
+heroku run rake db:populate_rel_start_end_date_20001_20367 --app sdfb2
+heroku run rake db:populate_rel_start_end_date_20368_40000 --app sdfb2
+heroku run rake db:populate_rel_start_end_date_50293_60000 --app sdfb2
+
