fbdcd7ec7bef40d192629e19861cb91a0da24d95
Katarina <katarinashaw@gmail.com>
2015-06-07 17:01:51 -0400

    added two new configuration settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,10 +36,16 @@ Sdfb::Application.configure do
   # Do not compress assets
   config.assets.compress = true
   config.serve_static_assets = true
+
   # Expands the lines which load the assets
   config.assets.debug = false
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( jquery-2.1.1.min jquery-impromptu.min.js jquery-ui.min.js accordion.js autocomplete-rails.js bootstrap.min display.js insights.standalone.js script.js pace.js typeahead.js )
 
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 end
