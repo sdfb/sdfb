d951d9cce721b2ff4c1d0d402d28078a1e122941
Katarina <katarinashaw@gmail.com>
2015-07-07 01:15:59 -0400

    added password digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20150707044839) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -103,11 +105,6 @@
     t.string   "start_date_type"
     t.string   "end_date_type"
     t.text     "last_edit",       default: "--- []\n"
-  end
-
-  create_table "password_digests", force: true do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "people", force: true do |t|
@@ -296,10 +293,10 @@
     t.string   "username"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.string   "auth_token"
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
-    t.string   "password_digest"
-    t.string   "auth_token"
+    t.text     "password_digest"
   end
 
 end
