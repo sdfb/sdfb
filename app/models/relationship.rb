cb9300c49cfcc7405b12e966ada942958ad4f600
jotis <jotis@bakerba910.wv.cc.cmu.edu>
2015-08-13 17:39:41 -0400

    relationship update dating error bug fixed

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -289,8 +289,8 @@ class Relationship < ActiveRecord::Base
       #if there are existing dates, check them
 
       #first make sure that the end year comes before the start year
-      if self.end_year.to_i > self.start_year.to_i
-        errors.add(:start_year, "The start year must less than or equal to the end year")
+      if self.end_year.to_i < self.start_year.to_i
+        errors.add(:start_year, "The start year must be less than or equal to the end year")
         errors.add(:end_year, "The end year must be greater than or equal to the start year")
       else
         # if end year is outside of the date range, check that there is 
