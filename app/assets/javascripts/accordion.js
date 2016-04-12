5e2c1bf840a94e65b8a3b6ecf09a570e925c35fc
Max Harlynking <mharlynk@andrew.cmu.edu>
2016-04-12 00:02:04 -0400

    Hid pieces of main menu

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -10,9 +10,48 @@ $(document).ready(function(){
 		}
 	});
 
-	// $("#filterBar").mouseenter(function(){
-	// 	$("#filterBar").css("opacity", 1);
-	// });
+	$("#filterBar a").click(function(e) {
+	   e.stopPropagation();
+	})
+
+	$("#filterBar").click(function(){
+		if ($("#filterBar").css("top") == "0px" || $("#filterBar").css("top") == "auto"){
+			$("#filterBar").animate({
+				top: "-70"
+			}, 300);
+			$("#zoom").animate({
+				top: "50"
+			}, 300);
+		}
+	});
+
+	$("#filterBar").mouseenter(function(){
+		if ($("#filterBar").css("top") == "-70px"){
+			$("#filterBar").animate({
+				top: "0"
+			}, 300);
+			$("#zoom").animate({
+				top: "120"
+			}, 300);
+		}
+	});
+
+
+	$(".close-right").click(function(){
+		if ($("#accordion").css("margin-left") == "0px"){
+			$("#accordion").animate({
+				marginLeft: "-300"
+			}, 600);
+			$(".close-right p").css("transform", "translateY(-50%) rotateY(180deg)");
+		}
+		else{
+			$("#accordion").animate({
+				marginLeft: "0"
+			}, 600);
+			$(".close-right p").css("transform", "translateY(-50%) rotateY(0deg)");
+		}
+	});
+
 });
 
 // When a user clicks on a node or edge.
