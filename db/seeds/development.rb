e434c5ab0d664064612a2c190ade4e1ad79bce7e
David Newbury <david.newbury@gmail.com>
2017-03-10 16:03:52 -0500

    Added relationship types and categories.  Also rebuilt the rel_types to pass validation.

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -7,11 +7,11 @@ sdfb_admin_id = User.first.id
 # Add all of the Group Category data from the dump
 CSV.foreach(Rails.root.join('lib', 'data', 'group_categories.tsv'), tsv_options) do |line|
   GroupCategory.where(
-    id: line[0],
-    name: line[1],
-    is_approved: true,
-    created_by: sdfb_admin_id,
-    approved_by: sdfb_admin_id.to_s
+    id:              line[0],
+    name:            line[1],
+    is_approved:     true,
+    created_by:      sdfb_admin_id,
+    approved_by:     sdfb_admin_id.to_s
   ).first_or_create!
 end
 
@@ -26,10 +26,34 @@ CSV.foreach(Rails.root.join('lib', 'data', 'groups.tsv'), tsv_options) do |line|
     start_date_type: line[3].present? ? "IN" : "CA",
     end_year:        line[4] || 1700,
     end_date_type:   line[4].present? ? "IN" : "CA",
-    created_by: sdfb_admin_id,
-    approved_by: sdfb_admin_id.to_s
+    created_by:      sdfb_admin_id,
+    approved_by:     sdfb_admin_id.to_s
   ).first_or_create!
 end
+
+# Add all of the Relationship Category data from the dump
+CSV.foreach(Rails.root.join('lib', 'data', 'rel_categories.tsv'), tsv_options) do |line|
+  RelationshipCategory.where(
+    id:              line[0],
+    name:            line[1],
+    is_approved:     true,
+    created_by:      sdfb_admin_id,
+    approved_by:     sdfb_admin_id.to_s,
+  ).first_or_create!
+end
+
+# Add all of the Relationship Type data from the dump
+CSV.foreach(Rails.root.join('lib', 'data', 'rel_types.tsv'), tsv_options) do |line|
+  RelationshipType.where(
+    id:              line[0],
+    name:            line[1],
+    relationship_type_inverse: line[2],
+    is_approved:     true,
+    created_by:      sdfb_admin_id,
+    approved_by:     sdfb_admin_id.to_s,
+  ).first_or_create!
+end
+
 
 # Preload some important people.
 francis = Person.where(
