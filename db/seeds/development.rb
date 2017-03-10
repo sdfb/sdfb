f2217ff608737fd8147658fcbe17ed3821b2f2c7
Justin Reese <justin.x.reese@gmail.com>
2017-03-10 15:21:09 -0500

    Adds Anne/Francis Bacon seeded relationship

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,10 +1,37 @@
-Person.create!(
+creator_id = User.first.id
+
+francis = Person.where(
+  id: 10000473,
   first_name: 'Francis',
   last_name: 'Bacon',
-  created_by: User.first.id,
+  created_by: creator_id,
   gender: 'male',
   birth_year_type: 'IN',
-  ext_birth_year: 1561,
+  ext_birth_year: '1561',
   death_year_type: 'IN',
-  ext_death_year: 1626
-)
+  ext_death_year: '1626',
+  approved_by: creator_id,
+  is_approved: true
+).first_or_create!
+
+anne = Person.where(
+  first_name: 'Anne',
+  last_name: 'Bacon',
+  created_by: creator_id,
+  gender: 'female',
+  birth_year_type: 'IN',
+  ext_birth_year: '1528',
+  death_year_type: 'IN',
+  ext_death_year: '1610',
+  approved_by: creator_id,
+  is_approved: true
+).first_or_create!
+
+anne_and_francis_relationship = Relationship.where(
+  person1_index: anne.id,
+  person2_index: francis.id,
+  original_certainty: 100,
+  created_by: User.first,
+  approved_by: creator_id,
+  is_approved: true
+).first_or_create!
