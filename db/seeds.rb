f12edf8405643495304a36eb5533b9bec44af387
Justin Reese <justin.x.reese@gmail.com>
2017-03-10 15:54:44 -0500

    Adds default administrator and curator users

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,10 @@ required_user_atts = {
 }
 User.where(email: 'sdfbadmin@example.com', username: 'sdfbadmin').first_or_create!(required_user_atts)
 User.where(email: 'odnbadmin@example.com', username: 'odnbadmin').first_or_create!(required_user_atts)
+required_user_atts.merge!({user_type: 'Curator'})
+User.where(email: 'curator@example.com', username: 'curator').first_or_create!(required_user_atts)
+required_user_atts.merge!({user_type: 'Admin'})
+User.where(email: 'administrator@example.com', username: 'administrator').first_or_create!(required_user_atts)
 
 seed_file = Rails.root.join( 'db', 'seeds', "#{Rails.env.downcase}.rb")
 
