0d5cd01995d432d79dd92a681cc21293470e27c3
David Newbury <david.newbury@gmail.com>
2017-03-17 15:53:47 -0400

    Tidies user_group_contrib model

--- app/models/user_group_contrib.rb
+++ app/models/user_group_contrib.rb
@@ -14,19 +14,19 @@ class UserGroupContrib < ActiveRecord::Base
 
   # Validations
   # -----------------------------
-  validates_presence_of :annotation
   validates_presence_of :created_by
   validates_presence_of :group_id
-  validates_length_of   :annotation, :minimum => 10, :if => :annot_present?
-  validates_length_of   :bibliography, :minimum => 10, :if => :bib_present?
+  validates_presence_of :annotation
+  validates_length_of   :annotation,   :minimum => 10
+  validates_length_of   :bibliography, :minimum => 10, allow_blank: true
 
   # Scope
   # ----------------------------- 
-  scope :for_user, -> ( user_input) { where('created_by = ?', "#{user_input}") }
-  scope :all_for_group, -> (groupID) {
-      select('user_group_contribs.*')
-      .where('group_id = ?', groupID)}
-  scope :all_recent, -> { order(updated_at: :desc) }
+  scope :for_user,         -> ( user_input) { where('created_by = ?', "#{user_input}") }
+  scope :all_for_group,    -> (groupID) {
+                                        select('user_group_contribs.*')
+                                        .where('group_id = ?', groupID)}
+  scope :all_recent,       -> { order(updated_at: :desc) }
   scope :order_by_sdfb_id, -> { order(id: :asc) }
 
   # Callbacks
@@ -36,24 +36,17 @@ class UserGroupContrib < ActiveRecord::Base
   # Custom Methods
   # -----------------------------
 
-  def get_group_name
-    return Group.find(group_id)
-  end
+  ### The two methods below are never called.  Confirm they can be removed.  -DGN 2017-3-17
 
+  # def get_group_name
+  #   return Group.find(group_id)
+  # end
 
-  def annot_present?
-    !self.annotation.blank?
-  end
-
-  def bib_present?
-    !self.bibliography.blank?
-  end
-
-  def get_users_name
-    if (created_by != nil)
-      return User.find(created_by).first_name + " " + User.find(created_by).last_name
-    else
-      return "ODNB"
-    end
-  end
+  # def get_users_name
+  #   if (created_by != nil)
+  #     return User.find(created_by).first_name + " " + User.find(created_by).last_name
+  #   else
+  #     return "ODNB"
+  #   end
+  # end
 end
