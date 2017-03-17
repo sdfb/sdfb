f82de69a4126eb301b2169bd2af5b65b33ebf14f
David Newbury <david.newbury@gmail.com>
2017-03-17 16:12:21 -0400

    Tidies rel_cat_assign.rb

--- app/models/rel_cat_assign.rb
+++ app/models/rel_cat_assign.rb
@@ -15,18 +15,15 @@ class RelCatAssign < ActiveRecord::Base
   # -----------------------------
   validates_presence_of :relationship_category_id
   validates_presence_of :relationship_type_id
-  # Commented out instead of deleted. Needs further evaluation in a cleanup of the app.
-  #validate :check_if_approved_valid_create, on: :create
-  validate :check_if_approved_and_update_edit, on: :update
 
   # Scope
   # ----------------------------- 
 
-  scope :for_rel_cat, -> (rel_cat_id_input) { where('relationship_category_id = ?', "#{rel_cat_id_input}") }
-  scope :for_rel_type, -> (rel_type_id_input) { where('relationship_type_id = ?', "#{rel_type_id_input}") }
-  scope :find_if_exists, -> (rel_cat_id_input, rel_type_id_input) { where('(relationship_category_id = ?) and (relationship_type_id = ?)', rel_cat_id_input, rel_type_id_input) }
-  scope :all_recent, -> { order(updated_at: :desc) }
-  scope :for_user, -> (user_input) { where('created_by = ?', "#{user_input}") }
+  scope :for_rel_cat,      -> (rel_cat_id_input) { where('relationship_category_id = ?', "#{rel_cat_id_input}") }
+  scope :for_rel_type,     -> (rel_type_id_input) { where('relationship_type_id = ?', "#{rel_type_id_input}") }
+  scope :find_if_exists,   -> (rel_cat_id_input, rel_type_id_input) { where('(relationship_category_id = ?) and (relationship_type_id = ?)', rel_cat_id_input, rel_type_id_input) }
+  scope :all_recent,       -> { order(updated_at: :desc) }
+  scope :for_user,         -> (user_input) { where('created_by = ?', "#{user_input}") }
   scope :order_by_sdfb_id, -> { order(id: :asc) }
 
   # Callbacks
@@ -37,14 +34,12 @@ class RelCatAssign < ActiveRecord::Base
   # Custom Methods
   # -----------------------------
 
-
   def check_if_already_exists
     if RelCatAssign.find_if_exists(self.relationship_category_id, self.relationship_type_id)
       errors.add(:relationship_type_id, "This relationship type is already assigned to this relationship category.")
     end
   end
 
-  # TODO: The logic here appears backwards.  Confirm that it should be the same as above?
   def check_if_duplicate
     search_results_for_duplicate = RelCatAssign.find_if_exists(self.relationship_category_id, self.relationship_type_id)
     if search_results_for_duplicate
@@ -53,4 +48,5 @@ class RelCatAssign < ActiveRecord::Base
       end
     end
   end
+
 end
