5ea010b2c561b7fbe8cf0b304faab4c6185b3ab9
David Newbury <david.newbury@gmail.com>
2017-03-17 16:02:47 -0400

    Tidies model for user_person_contrip

--- app/models/user_person_contrib.rb
+++ app/models/user_person_contrib.rb
@@ -14,48 +14,40 @@ class UserPersonContrib < ActiveRecord::Base
 
   # Validations
   # -----------------------------
-  validates_presence_of :annotation
   validates_presence_of :created_by
   validates_presence_of :person_id
-  ## annotation must be at least 10 characters
-  validates_length_of :annotation, :minimum => 10, :if => :annot_present?
-  ## bibliography must be at least 10 characters
-  validates_length_of :bibliography, :minimum => 10, :if => :bib_present?
+  validates_presence_of :annotation
+  validates_length_of   :annotation, :minimum => 10
+  validates_length_of   :bibliography, :minimum => 10, allow_blank: true
 
   # Scope
   # ----------------------------- 
-  scope :for_user, -> (user_input) { where('created_by = ?', "#{user_input}") }
-  scope :all_for_person, -> (personID) {
-      select('user_person_contribs.*')
-      .where('person_id = ?', personID)}
-  scope :all_recent, -> { order(updated_at: :desc) }
+  scope :for_user,         -> (user_input) { where('created_by = ?', "#{user_input}") }
+  scope :all_for_person,   -> (personID) {
+                                          select('user_person_contribs.*')
+                                          .where('person_id = ?', personID)}
+  scope :all_recent,       -> { order(updated_at: :desc) }
   scope :order_by_sdfb_id, -> { order(id: :asc) }
 
   # Callbacks
   # ----------------------------- 
-  before_save { remove_trailing_spaces(:annotation, :bibliography)}
+  before_save { remove_trailing_spaces(:annotation, :bibliography) }
 
 
   # Custom Methods
   # -----------------------------
 
-  def annot_present?
-    !annotation.blank?
-  end
+  ### The two methods below are never called.  Confirm they can be removed.  -DGN 2017-3-17
 
-  def bib_present?
-    !bibliography.blank?
-  end
+  # def get_person_name
+  #   return Person.find(person_id).display_name 
+  # end
 
-  def get_person_name
-    return Person.find(person_id).display_name 
-  end
-
-  def get_users_name
-    if (created_by != nil)
-      return User.find(created_by).first_name + " " + User.find(created_by).last_name
-    else
-      return "ODNB"
-    end
-  end
+  # def get_users_name
+  #   if (created_by != nil)
+  #     return User.find(created_by).first_name + " " + User.find(created_by).last_name
+  #   else
+  #     return "ODNB"
+  #   end
+  # end
 end
