588f4275929f0284d1b76a9429a4b52f8dd49811
JR Ladd <johnrobertladd@gmail.com>
2017-04-13 17:39:48 -0400

    implemented DD styles in person_hooke

--- person_hooke/person_hooke.js
+++ person_hooke/person_hooke.js
@@ -18,8 +18,11 @@ svg.append('rect')
       if (toggle == 1) {
         // Restore nodes and links to normal opacity. (see toggleClick() below)
         d3.selectAll('.link').style('stroke', '#000');
-        d3.select('[pulse="true"]').transition().duration(200).style('opacity', 1);
-        d3.selectAll('.node').style('fill', function(d) { return color(d.distance); }).attr('pulse', false);
+        // d3.select('[pulse="true"]').transition().duration(200).style('opacity', 1);
+        d3.selectAll('.node')
+          // .attr('pulse', false)
+          .classed('faded', false)
+          .classed('focused', false);
         d3.selectAll('span').remove();
         toggle = 0;
       }
@@ -33,9 +36,9 @@ var container = svg.append('g');
 // Create form for search (see function below).
 var search = d3.select("div#tools").append('form').attr('onsubmit', 'return false;');
 
-var color = d3.scaleOrdinal()
-    .domain([0,1,2])
-    .range(['#1253a0','#1253a0','#87a9cf']);
+// var color = d3.scaleOrdinal()
+//     .domain([0,1,2])
+//     .range(['#F45D4C','#DF947A','#D4D5D6']);
 
 
 
@@ -81,14 +84,14 @@ d3.json("baconnetwork.json", function(error, json) {
 
   degreeSize = d3.scaleLog()
       .domain([d3.min(graph.nodes, function(d) {return d.degree; }),d3.max(graph.nodes, function(d) {return d.degree; })])
-      .range([10,35]);
+      .range([10,45]);
 
   var simulation = d3.forceSimulation(graph.nodes)
       // .velocityDecay(.5)
       .force("link", d3.forceLink(graph.links).id(function(d) { return d.id; }))
       .force("charge", d3.forceManyBody().strength([-300]))//.distanceMax([500]))
       .force("center", d3.forceCenter(width / 2, height / 2))
-      .force("collide", d3.forceCollide().radius( function (d) { return degreeSize(d.degree); }))
+      .force("collide", d3.forceCollide().radius( function (d) { return degreeSize(d.degree) + 1; }))
       .force("x", d3.forceX())
       .force("y", d3.forceY())
       .stop();
@@ -247,16 +250,16 @@ function update(threshold, complexity) {
   node.exit().remove();
   var nodeEnter = node.enter().append('circle');
 
-  node.attr('r', function(d) { return degreeSize(d.degree);});  
+  node.attr('r', function(d) { return degreeSize(d.degree);});
 
   node = nodeEnter.merge(node)
     .attr('r', function(d) { return degreeSize(d.degree);})
-    .attr("fill", function(d) { return color(d.distance); })
-    .attr('class', 'node')
+    // .attr("fill", function(d) { return color(d.distance); })
+    .attr('class', function(d){ return 'node degree'+d.distance })
     .attr('id', function(d) { return "n" + d.id.toString(); })
     .attr("cx", function(d) { return d.x; })
     .attr("cy", function(d) { return d.y; })
-    .attr("pulse", false)
+    // .attr("pulse", false)
     .attr("is_source", function(d) {if (d.id == sourceId) {return 'true';} })
     // On click, toggle ego networks for the selected node. (See function above.)
     .on('click', function(d) { toggleClick(d, newLinks); });
@@ -264,12 +267,12 @@ function update(threshold, complexity) {
     node.append("title")
         .text(function(d) { return d.name; });
 
-  d3.select('.nodes').insert('circle', '[is_source="true"]')
-    .attr('r', degreeSize(d3.select('[is_source="true"]').data()[0].degree) + 10)
-    .attr('fill', color(d3.select('[is_source="true"]').data()[0].distance))
-    .attr('class', 'source-node')
-    .attr("cx", d3.select('[is_source="true"]').data()[0].x)
-    .attr("cy", d3.select('[is_source="true"]').data()[0].y);
+  // d3.select('.nodes').insert('circle', '[is_source="true"]')
+  //   .attr('r', degreeSize(d3.select('[is_source="true"]').data()[0].degree) + 10)
+  //   .attr('fill', color(d3.select('[is_source="true"]').data()[0].distance))
+  //   .attr('class', 'source-node')
+  //   .attr("cx", d3.select('[is_source="true"]').data()[0].x)
+  //   .attr("cy", d3.select('[is_source="true"]').data()[0].y);
 }
 
 // A function to handle click toggling based on neighboring graph.nodes.
@@ -285,18 +288,23 @@ function toggleClick(d, newLinks) {
    });
 
       if (toggle == 0) {
-        recursivePulse(d);
+        // recursivePulse(d);
         // Ternary operator restyles links and nodes if they are adjacent.
         d3.selectAll('.link').style('stroke', function (l) {
           return l.target == d || l.source == d ? 1 : '#D3D3D3';
         });
-        d3.selectAll('.node').style('fill', function (n) {
-          if (n.id in connectedNodes) { return color(n.distance); }
-          else { return '#D3D3D3'; };
-        });
+        d3.selectAll('.node')
+           .classed('faded', function(n){
+             if (n.id in connectedNodes) { return false }
+             else { return true; };
+           })
+           .classed('focussed', function(n){
+             if (n.id in connectedNodes) { return true }
+             else { return false; };
+           })
 
     // Show information when node is clicked
-    d3.select('div#tools').append('span').text("Name: " + d.name + "  |  Historical Significance: " + d.historical_significance + "  |  Lived: " + d.birth_year + "-" + d.death_year);
+    // d3.select('div#tools').append('span').text("Name: " + d.name + "  |  Historical Significance: " + d.historical_significance + "  |  Lived: " + d.birth_year + "-" + d.death_year);
         toggle = 1;
       }
 }
@@ -328,15 +336,15 @@ function linkArc(d) {
   return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
 }
 
-function recursivePulse(d) {
-  pulse();
-
-  function pulse() {
-    d3.select('#n' + d.id.toString())
-    .attr('pulse', true)
-    .transition().duration(1500).style('opacity', .5)
-    .transition().duration(1500).style('opacity', 1)
-    .on('end', pulse);
-  }
-
-}
+// function recursivePulse(d) {
+//   pulse();
+//
+//   function pulse() {
+//     d3.select('#n' + d.id.toString())
+//     .attr('pulse', true)
+//     .transition().duration(1500).style('opacity', .5)
+//     .transition().duration(1500).style('opacity', 1)
+//     .on('end', pulse);
+//   }
+//
+// }
