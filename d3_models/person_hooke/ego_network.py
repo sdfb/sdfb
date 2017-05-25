709a436963e5f3dfbff585c8ffe9d47d0539b00e
JR Ladd <johnrobertladd@gmail.com>
2017-05-25 10:15:27 -0400

    updated JSON for person_hooke

--- d3_models/person_hooke/ego_network.py
+++ d3_models/person_hooke/ego_network.py
@@ -109,33 +109,35 @@ SG = G.subgraph(all_nodes)
 # Create a dictionary for the JSON needed by D3.
 new_data = dict(
         data=dict(
-            type='networks',
-            id='1',
+            type='network',
+            id=str(bacon),
             attributes=dict(
-                nodes=[dict(
-                    id=n,
-                    person_info=dict(name=SG.node[n]['name'], groups=SG.node[n]['groups']),
-                    degree=SG.node[n]['degree']) for n in SG.nodes()],
-                links=[dict(
-                    source=e[0],
-                    target=e[1],
-                    weight=e[2]['weight'],
-                    altered=e[2]['altered'],
-                    start_year=e[2]['start_year'],
-                    end_year=e[2]['end_year'],
-                    id=e[2]['edge_id']) for e in SG.edges(data=True)])),
-        errors=[dict(
-            status='404',
-            title='Page not found')],
+                primary_people = [str(bacon)],
+                connections=[dict(
+                    id = e[2]['edge_id'],
+                    type="relationship",
+                    attributes=dict(
+                        source=e[0],
+                        target=e[1],
+                        weight=e[2]['weight'],
+                        altered=e[2]['altered'],
+                        start_year=e[2]['start_year'],
+                        end_year=e[2]['end_year']
+                    )
+                ) for e in SG.edges(data=True)])),
         meta=dict(
             principal_investigators=['Daniel Shore', 'Chris Warren', 'Jessica Otis']),
-        included=dict(
-            type='people',
-            id=str(bacon),
-            name=SG.node[bacon]['name'],
-            historical_significance=SG.node[bacon]['historical_significance'],
-            birth_year=SG.node[bacon]['birth_year'],
-            death_year=SG.node[bacon]['death_year'])
+        included=[dict(
+            id=n,
+            type="person",
+            attributes = dict(
+                name=SG.node[n]['name'],
+                degree=SG.node[n]['degree'],
+                historical_significance=SG.node[n]['historical_significance'],
+                birth_year=SG.node[n]['birth_year'],
+                death_year=SG.node[n]['death_year'],
+                groups=SG.node[n]['groups']
+            )) for n in SG.nodes()]
         )
 # Output json of the graph.
 with open('baconnetwork.json', 'w') as output:
