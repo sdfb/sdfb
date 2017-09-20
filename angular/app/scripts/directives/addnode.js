76f5c7faa084cbb1a47fd86e5ba3f4172b72f3da
JR Ladd <johnrobertladd@gmail.com>
2017-09-20 10:50:53 -0400

    add nodes when not in view

--- angular/app/scripts/directives/addnode.js
+++ angular/app/scripts/directives/addnode.js
@@ -15,17 +15,13 @@ angular.module('redesign2017App')
         // element.text('this is the addNode directive');
         scope.newNode.birthDateType = scope.newNode.deathDateType = scope.config.dateTypes[1];
 
-        // if (scope.noResultsPersonAdd) {
-        //   scope.newNode = {};
-        //   scope.newNode.exists = false;
-        // }
-
         scope.watch('noResultsPersonAdd', function(newValue, oldValue) {
           // scope.noResults = newValue;
           if (newValue) {
             scope.newNode.exists = false;
+            scope.notInView = true;
           }
-        })
+        });
 
         // When canvas is clicked, add a new circle with dummy data
         scope.addNode = function(addedNodes, point, update) {
@@ -70,26 +66,48 @@ angular.module('redesign2017App')
             scope.notInView = true;
           }
         }
+
+        function checkForNameless(arr) {
+          arr.forEach(function(a) {
+            if (!a['name']) {
+              return true;
+            }
+          });
+        }
         scope.submitNode = function() {
           console.log("node submitted");
           var newNode = angular.copy(scope.newNode);
 
           if (scope.config.viewMode === 'individual-force' ||  scope.config.viewMode === 'individual-concentric') {
-            if (scope.addedNodes.length > 0 && !scope.addedNodes[scope.addedNodes.length-1].attributes.name) {
+            if (scope.notInView === true && scope.addedNodes.length > 0 && !scope.addedNodes[scope.addedNodes.length-1].attributes.name) {
               scope.addedNodes[scope.addedNodes.length-1].attributes.name = newNode.name;
+            }
+            else if (scope.notInView === true && (scope.addedNodes.length === 0 || !checkForNameless(scope.addedNodes))) {
+              var newNode = { attributes: { name: scope.newNode.name, degree: 1 }, id: 0, distance: 7, x: scope.singleWidth/2, y: scope.singleHeight/2};
+              scope.addedNodes.push(newNode);
             }
             scope.updatePersonNetwork(scope.data);
           } else if (scope.config.viewMode === 'shared-network') {
             if (scope.addedSharedNodes.length > 0 && !scope.addedSharedNodes[scope.addedSharedNodes.length-1].attributes.name) {
               scope.addedSharedNodes[scope.addedSharedNodes.length-1].attributes.name = newNode.name;
             }
+            else if (scope.notInView === true && (scope.addedSharedNodes.length === 0 || !checkForNameless(scope.addedSharedNodes))) {
+              var newNode = { attributes: { name: scope.newNode.name, degree: 1 }, id: 0, distance: 7, x: scope.singleWidth/2, y: scope.singleHeight/2};
+              scope.addedSharedNodes.push(newNode);
+            }
             scope.updateSharedNetwork(scope.data);
           } else if (scope.config.viewMode === 'group-force') {
             if (scope.addedGroupNodes.length > 0 && !scope.addedGroupNodes[scope.addedGroupNodes.length-1].attributes.name) {
               scope.addedGroupNodes[scope.addedGroupNodes.length-1].attributes.name = newNode.name;
             }
+            else if (scope.notInView === true && (scope.addedGroupNodes.length === 0 || !checkForNameless(scope.addedGroupNodes))) {
+              var newNode = { attributes: { name: scope.newNode.name, degree: 1 }, id: 0, distance: 7, x: scope.singleWidth/2, y: scope.singleHeight/2};
+              scope.addedGroupNodes.push(newNode);
+            }
             scope.updateGroupNetwork(scope.data);
           }
+
+          if (scope.notInView === true)
 
           if (!scope.newNode.exists) { scope.addToDB.nodes.push(newNode); }
           scope.addNodeClosed = true;
