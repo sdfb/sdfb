53ff80452a67a77c4db64cddd834d970d76dbba4
JR Ladd <johnrobertladd@gmail.com>
2017-10-10 16:06:37 -0400

    dropdown for reltype

--- angular/app/scripts/directives/addlink.js
+++ angular/app/scripts/directives/addlink.js
@@ -12,6 +12,53 @@ angular.module('redesign2017App')
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         scope.newLink.startDateType = scope.newLink.endDateType = scope.config.dateTypes[1];
+        scope.config.relTypes = [{'name': 'Enemy of', 'id': 1},
+              {'name': 'Rival of', 'id': 2},
+              {'name': 'Acquaintance of', 'id': 3},
+              {'name': 'Met', 'id': 4},
+              {'name': 'Knew in passing', 'id': 5},
+              {'name': 'Close friend of', 'id': 6},
+              {'name': 'Friend of', 'id': 7},
+              {'name': 'Attracted to/Admired by', 'id': 8},
+              {'name': 'Had child with', 'id': 9},
+              {'name': 'Sexual partner of', 'id': 10},
+              {'name': 'Pupil/Student of-Mentor/Teacher of', 'id': 11},
+              {'name': 'Mentor/Teacher of-Pupil/Student of', 'id': 12},
+              {'name': 'Aunt/Uncle of-Niece/Nephew of', 'id': 13},
+              {'name': 'Cousin of', 'id': 14},
+              {'name': 'Child-in-law of/Parent-in-law of', 'id': 16},
+              {'name': 'Guardian of/Ward of', 'id': 17},
+              {'name': 'Heir(ess) of/Testator to', 'id': 18},
+              {'name': 'Other-in-law of/Other-in-law of', 'id': 19},
+              {'name': 'Sibling-in-law of', 'id': 21},
+              {'name': 'Great-grandparent of/Great-grandchild of', 'id': 24},
+              {'name': 'Child of/Parent of', 'id': 25},
+              {'name': 'Grandchild of/Grandparent of', 'id': 27},
+              {'name': 'Sibling of', 'id': 30},
+              {'name': 'Godchild of/Godparent of', 'id': 31},
+              {'name': 'Godsibling of', 'id': 33},
+              {'name': 'Client of/Patron of', 'id': 34},
+              {'name': 'Collaborated with', 'id': 35},
+              {'name': 'Colleague of', 'id': 36},
+              {'name': 'Coworker of', 'id': 37},
+              {'name': 'Apprentice of/Master of', 'id': 39},
+              {'name': 'Employed by/Employer of', 'id': 40},
+              {'name': 'Rented from/Rented to', 'id': 43},
+              {'name': 'Engaged to', 'id': 45},
+              {'name': 'Spouse of', 'id': 46},
+              {'name': 'Parishoner of/Priest of', 'id': 47},
+              {'name': 'Other religious relationship', 'id': 49},
+              {'name': 'Lived with', 'id': 50},
+              {'name': 'Neighbor of', 'id': 51},
+              {'name': 'Step-parent of/Step-child of', 'id': 103},
+              {'name': 'Step-sibling of', 'id': 105},
+              {'name': 'Creditor of/Debtor of', 'id': 106},
+              {'name': 'Schoolmate of', 'id': 108},
+              {'name': 'Correspondent of', 'id': 109},
+              {'name': 'Midwife for/Had as midwife', 'id': 110},
+              {'name': 'Attendant of/Attended by', 'id': 112}]
+        scope.newLink.relType = scope.config.relTypes[3];
+        console.log(scope.config.relTypes[3].name);
         scope.newLink.source = {};
         scope.newLink.target = {};
         scope.slider = {
@@ -198,11 +245,6 @@ angular.module('redesign2017App')
             scope.newLink.source.id = args.source.id;
             scope.newLink.target.name = args.target.attributes.name;
             scope.newLink.target.id = args.target.id;
-            // d3.select('#startDate').property('value', Math.max(source.attributes.birth_year, target.attributes.birth_year));
-            // d3.select('#startDateType').property('value', 'After/On');
-            // d3.select("#endDate").property('value', Math.min(source.attributes.death_year, target.attributes.death_year));
-            // d3.select("#endDateType").property('value', 'Before/On');
-            // d3.select('#relType').property('value', 'has met');
             scope.addLinkClosed = false;
           }
         })
