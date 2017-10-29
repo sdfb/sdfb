6e6f7592ced3a6cb304f618ad4a0a8438f3f11aa
David Newbury <david.newbury@gmail.com>
2017-10-29 14:48:27 -0700

    Added the "is_public" field.  This should close #10.

--- rails/app/controllers/api_controller.rb
+++ rails/app/controllers/api_controller.rb
@@ -26,7 +26,7 @@ class ApiController < ApplicationController
   def edit_user
     return head(:forbidden) unless current_user
     return head(:forbidden) unless params["id"].to_s == current_user.id.to_s || current_user.user_type == "Admin"
-    fields = %w{about_description affiliation email first_name last_name username prefix orcid}
+    fields = %w{about_description affiliation email first_name last_name username prefix orcid is_public}
     fields += %w{is_active user_type} if current_user.user_type == "Admin"
     fields += %w{password password_confirmation} if params["id"].to_s == current_user.id.to_s
     new_record = fields.collect{|field| [field, params[field]] if params[field]}.compact.to_h
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029200631) do
+ActiveRecord::Schema.define(version: 20171029214412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -316,12 +316,13 @@ ActiveRecord::Schema.define(version: 20171029200631) do
     t.string   "orcid"
     t.integer  "created_by"
     t.string   "username"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "auth_token"
     t.string   "password_digest"
     t.string   "password_reset_sent_at"
     t.string   "password_reset_token"
+    t.boolean  "is_public",              default: false
   end
 
 end
