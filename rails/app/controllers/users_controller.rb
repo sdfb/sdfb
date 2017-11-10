8b77886a21be11cd2526c0c1f2cc61af6c377a47
David Newbury <david.newbury@gmail.com>
2017-11-10 16:58:57 -0500

    deleted signup and password reset interfaces

--- rails/app/controllers/users_controller.rb
+++ rails/app/controllers/users_controller.rb
@@ -48,17 +48,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/new
-  # GET /users/new.json
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
-  end
-
   # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -40,6 +40,4 @@ Sdfb::Application.routes.draw do
   get "sign_out" => "sessions#destroy", :as => "sign_out"
 
   resources :users
-  get 'password_resets/new'
-  get "sign_up" => "users#new", :as => "sign_up"
 end
