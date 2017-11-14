dd5e71fea7e723435d8e7ce05492b76beae79231
David Newbury <david.newbury@gmail.com>
2017-11-13 19:18:35 -0800

    Removed GroupCategory table

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114031555) do
+ActiveRecord::Schema.define(version: 20171114031743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,19 +50,6 @@ ActiveRecord::Schema.define(version: 20171114031555) do
     t.boolean  "is_active",                     default: true
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
-    t.text     "bibliography"
-  end
-
-  create_table "group_categories", force: :cascade do |t|
-    t.string   "name",         limit: 255
-    t.text     "description"
-    t.integer  "created_by"
-    t.string   "approved_by",  limit: 255
-    t.string   "approved_on",  limit: 255
-    t.boolean  "is_approved",              default: false
-    t.boolean  "is_active",                default: true
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
     t.text     "bibliography"
   end
 
