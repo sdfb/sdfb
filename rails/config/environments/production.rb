b5adf814ae8ccfa4a1b6d0747dd5ffda6fd0e663
JR Ladd <johnrobertladd@gmail.com>
2017-11-16 13:04:41 -0500

    email for password reset

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -76,9 +76,17 @@ Sdfb::Application.configure do
 #  }
 #  config.action_mailer.default_url_options = { :host =>  "sixdegfrancisbacon.herokuapp.com" }  
 #
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@sixdegressoffrancisbacon.com'}
+  #config.action_mailer.default_options = {from: 'no-reply@sixdegressoffrancisbacon.com'}
+  config.action_mailer.smtp_settings = {
+    :address	=> "smtp.gmail.com",
+    :port	=> 587,
+    :user_name  => Rails.application.secrets.gmail_username,
+    :password	=> Rails.application.secrets.gmail_password,
+    :authentication	=> "plain",
+  :enable_starttls_auto	=> true
+}
 
 end
